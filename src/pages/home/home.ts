252eba51d888668304f478ad1c5fad1190779bd2
taimoorAshfaq <35800623+taimoorAshfaq@users.noreply.github.com>
2019-02-12 12:26:10 +0500

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RegisterDoctorPage } from '../register-doctor/register-doctor';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  registerDoctor(){
+
+    this.navCtrl.push(RegisterDoctorPage);
   }
 
 }
