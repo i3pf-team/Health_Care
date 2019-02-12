6525a709e1c5fc7654850e1cfc29bfda948a75e3
zeesss <46367964+zeesss@users.noreply.github.com>
2019-02-12 15:06:53 +0500

    Update register-patient.ts

--- src/pages/register-patient/register-patient.ts
+++ src/pages/register-patient/register-patient.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {  NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { SelectCategoryPage} from '../select-category/select-category';
 
 /**
  * Generated class for the RegisterPatientPage page.
@@ -15,7 +17,20 @@ import {  NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPatientPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public toastController: ToastController, public navCtrl: NavController, public navParams: NavParams) {
+  }
+  Login(){
+
+    this.navCtrl.push(SelectCategoryPage);
+  }
+  presentToastWithOptions() {
+    const toast =  this.toastController.create({
+      message: 'User Registered!',
+      showCloseButton: true,
+      position: 'bottom',
+      closeButtonText: 'Done'
+    });
+    toast.present();
   }
 
   ionViewDidLoad() {
