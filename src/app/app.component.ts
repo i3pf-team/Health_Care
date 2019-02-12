5e8d5009abc5cb9471c821c14a0025b1a5f4e4b8
zeesss <46367964+zeesss@users.noreply.github.com>
2019-02-12 15:06:42 +0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { SelectCategoryPage } from '../pages/select-category/select-category';
 import { ListPage } from '../pages/list/list';
 
 @Component({
