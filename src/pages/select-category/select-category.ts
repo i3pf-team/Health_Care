4d730d418f55f4f6abdf412c4e093c9cc323f1a5
zeesss <46367964+zeesss@users.noreply.github.com>
2019-02-12 11:51:34 +0500

    Update select-category.ts

--- src/pages/select-category/select-category.ts
+++ src/pages/select-category/select-category.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {  NavController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the SelectCategoryPage page.
@@ -8,7 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
 @Component({
   selector: 'page-select-category',
   templateUrl: 'select-category.html',
