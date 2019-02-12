5800e034c0e3f5a31873de5a9a532ea358ec4c4a
taimoorAshfaq <35800623+taimoorAshfaq@users.noreply.github.com>
2019-02-12 12:40:51 +0500

    Update register-doctor.ts

--- src/pages/register-doctor/register-doctor.ts
+++ src/pages/register-doctor/register-doctor.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
+import { SelectCategoryPage } from '../select-category/select-category';
 
 /**
  * Generated class for the RegisterDoctorPage page.
@@ -31,13 +32,7 @@ export class RegisterDoctorPage {
 
   Login(){
 
-    const toast =  this.toastController.create({
-      message: 'Goto Login Page',
-      showCloseButton: true,
-      position: 'bottom',
-      closeButtonText: 'Done'
-    });
-    toast.present();
+    this.navCtrl.push(SelectCategoryPage);
   }
 
 
