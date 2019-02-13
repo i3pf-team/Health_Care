bbf6b2e54128328061a94375d5ab5e1b9f2481ed
m-jayy <m.mohsinjaved2010@gmail.com>
2019-02-13 16:56:37 +0500

    Create doctor-home.ts

---
+++ src/pages/doctor-home/doctor-home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the DoctorHomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+
+@Component({
+  selector: 'page-doctor-home',
+  templateUrl: 'doctor-home.html',
+})
+export class DoctorHomePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DoctorHomePage');
+  }
+
+}
